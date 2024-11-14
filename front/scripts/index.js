f6593dbf8f3b22bfa0e7bf427baed8efc1c17739
Daniel Esteban Bustos <daniel.kpo.342@gmail.com>
2024-11-14 11:53:28 -0300

    feat: :sparkles: aplicando una peticion get con jquery

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,29 +2,32 @@ const recomentationsContainer = document.querySelector(
   ".recomentations-container"
 );
 
-const htmlRecomentations = tempData.map((movie) => {
-  const {
-    title = "Titulo no disponible",
-    year = "Año desconocido",
-    director = "Director desconocido",
-    duration = "Duracion no especificada",
-    genre = "Genero no especificado",
-    rate = "Sin calificacion",
-    poster = "../assets/image/error-portada.jpg",
-  } = movie;
-  return `
-    <div class="recomentations">
-    <img src="${poster}">
-    <div class="recomentations-info">
-    <h2>${title}</h2>
-    <p>${rate}</p>
-    <p>geneno: ${genre}</p>
-    <p>duracion: ${duration}</p>
-    <p>año: ${year}</p>
-    <p>director: ${director}</p>
-    </div>
-    </div>
-    `;
-});
+$.get("https://students-api.up.railway.app/movies", (data)=>{
+  const htmlRecomentations = data.map((movie) => {
+    const {
+      title = "Titulo no disponible",
+      year = "Año desconocido",
+      director = "Director desconocido",
+      duration = "Duracion no especificada",
+      genre = "Genero no especificado",
+      rate = "Sin calificacion",
+      poster = "../assets/image/error-portada.jpg",
+    } = movie;
+    return `
+      <div class="recomentations">
+      <img src="${poster}">
+      <div class="recomentations-info">
+      <h2>${title}</h2>
+      <p>${rate}</p>
+      <p>geneno: ${genre}</p>
+      <p>duracion: ${duration}</p>
+      <p>año: ${year}</p>
+      <p>director: ${director}</p>
+      </div>
+      </div>
+      `;
+})
 
 recomentationsContainer.innerHTML = htmlRecomentations;
+});
+
