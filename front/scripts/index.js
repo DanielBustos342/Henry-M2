670570331a33c43f0ce5b103d8c73f2d89dc5b44
DanielBustos342 <daniel.kpo.342@gmail.com>
2024-11-15 23:27:02 -0300

    feat: :sparkles: Implementancion de boostrap
    agregar un carrousel

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,7 +2,7 @@ const recomentationsContainer = document.querySelector(
   ".recomentations-container"
 );
 
-$.get("https://students-api.up.railway.app/movies", (data)=>{
+$.get("https://students-api.up.railway.app/movies", (data) => {
   const htmlRecomentations = data.map((movie) => {
     const {
       title = "Titulo no disponible",
@@ -26,8 +26,54 @@ $.get("https://students-api.up.railway.app/movies", (data)=>{
       </div>
       </div>
       `;
-})
+  });
 
-recomentationsContainer.innerHTML = htmlRecomentations;
+  recomentationsContainer.innerHTML = htmlRecomentations;
 });
 
+const cardCarousel = document.getElementById("card-carousel");
+console.log(cardCarousel);
+
+$.get("https://students-api.up.railway.app/movies", (data) => {
+  const htmlCardCarousel = data
+    .map((movie, index) => {
+      const { title, genre, poster } = movie;
+      return `
+   <div class="carousel-item ${
+     index === 0 ? "active" : ""
+   }" style="background: linear-gradient(270deg, #FFF,10%, #000">
+      <div class="d-flex flex-row-reverse ">
+        <div class="carousel-caption" style="width: 40%; height: calc(100vh - 60px); display: flex; flex-direction: column; justify-content: center; align-items: start">
+          <h5>${title}</h5>
+          <p>${genre}</p>
+          <button class="btn-crunchy">Ver mas</button>
+        </div>
+        <div>
+          <img src="${poster}" class="d-block w-100 object-fit-contain" alt="..." style="height: calc(100vh - 60px);">
+        </div>
+      </div>
+   </div>
+   `;
+    })
+    .join("");
+  console.log(cardCarousel);
+  cardCarousel.innerHTML = htmlCardCarousel;
+});
+
+const carrouselIndicators = document.getElementById("carousel-indicators");
+$.get("https://students-api.up.railway.app/movies", (data) => {
+  const htmlCarouselIndicators = data
+    .map((_, index) => {
+      return `
+      <button 
+      type="button" 
+      data-bs-target="#carouselExampleCaptions" 
+      data-bs-slide-to="${index}" 
+      aria-label="Slide ${index + 1}"
+      class="active border rounded-3"
+      ></button>`;
+    })
+    .join("");
+  console.log(carrouselIndicators);
+  carrouselIndicators.innerHTML = htmlCarouselIndicators;
+});
