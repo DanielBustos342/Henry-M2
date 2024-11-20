bcd0fc40d32ce8db4c439ccfaef0ff7e5ea6cea2
Daniel Bustos <123027736+DanielBustos342@users.noreply.github.com>
2024-11-20 18:37:48 -0300

    feat: :sparkles: Vincular mongoose con el back

--- back/index.js
+++ back/index.js
@@ -1,5 +1,8 @@
 const app = require("./src/server.js");
+const dbCon = require("./src/config/dbCon.js");
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
+dbCon().then((res) => {
+  app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+  });
 });
